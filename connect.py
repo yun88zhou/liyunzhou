f597ef17c3b4f51b9315afb2557a31b3c15f29ef
Liyun Zhou <122519566+yun88zhou@users.noreply.github.com>
2023-10-01 17:04:21 +1300

    Add files via upload

---
+++ connect.py
@@ -0,0 +1,7 @@
+# These details are available on the first MySQL Workbench screen
+# Usually called 'Local Instance'
+dbuser = "root" # Your MySQL username - likely 'root'
+dbpass = "XXXXX" # ---- PUT YOUR PASSWORD HERE ----
+dbhost = "localhost" 
+dbport = "3306"
+dbname = "motorkhana"
