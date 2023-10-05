fd00b880408e1cdcd9befb0c84f01a5778e3a7f5
Liyun Zhou <122519566+yun88zhou@users.noreply.github.com>
2023-10-05 14:16:21 +1300

    Update connect.py
    corrected password

--- connect.py
+++ connect.py
@@ -1,3 +1,3 @@
 from db import DB
-db = DB(username="root", password="LYRICjunjin82619", hostname="localhost",
+db = DB(username="root", password="LYRICjunjin82619@", hostname="localhost",
             dbtype="motorkhana")
